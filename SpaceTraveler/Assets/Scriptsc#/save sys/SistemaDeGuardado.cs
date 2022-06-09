fae32e40324c379baa54a7c8d371352fdb0eae75
ivanH1945 <ivanhernandez02@aragon.unam.mx>
2022-06-09 17:04:40 -0500

    se eliminaron variables

--- SpaceTraveler/Assets/Scriptsc#/save sys/SistemaDeGuardado.cs
+++ SpaceTraveler/Assets/Scriptsc#/save sys/SistemaDeGuardado.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 public static class SistemaDeGuardado 
 {
-    public static bool dexist;
+   
 
     public static void saveplayerdata(Player player) // guarda datos
     {
@@ -13,13 +13,14 @@ public static class SistemaDeGuardado
         FileStream fileStream = new FileStream(datapath, FileMode.Create);
         BinaryFormatter binary = new BinaryFormatter();
         binary.Serialize(fileStream, data);
+        Debug.Log(datapath);
     }
     public static datosdeljugador loadPlayerData() 
     {
         string datapath = Application.persistentDataPath + "/player.xwing";
         if (File.Exists(datapath))
-        {
-            dexist = true;
+        {  
+         
             FileStream fileStream = new FileStream(datapath, FileMode.Open);
             BinaryFormatter binary = new BinaryFormatter();
             datosdeljugador playerdata = (datosdeljugador)binary.Deserialize(fileStream);
@@ -29,7 +30,7 @@ public static class SistemaDeGuardado
         }
         else 
         {
-            dexist = false;
+
 
             return null;
         }
