2117cc33d13a0ce0e24f2ceaa769fa7475529eca
ivanH1945 <ivanhernandez02@aragon.unam.mx>
2022-06-19 10:32:59 -0500

    pruebas de pausa en el planeta

--- SpaceTraveler/Assets/playeronfoot/scripts/Astronaut.cs
+++ SpaceTraveler/Assets/playeronfoot/scripts/Astronaut.cs
@@ -7,39 +7,55 @@ using UnityEngine.UI;
 public class Astronaut : MonoBehaviour
 {
 
-    
-  
+
+
     public GameObject pauseui;
     public bool paused;
-   
+
 
     [SerializeField]
     private Player player;
-
+    private void Start()
+    {
+        paused =false;
+    }
+    private void Update()
+    {
+        pausemenu();
+    }
     public void cargar()
     {
 
         datosdeljugador playerdata = SistemaDeGuardado.loadPlayerData();
-        
-       
+
+
         Debug.Log("Dataloaded");
 
     }
     public void guardar()
     {
-        SistemaDeGuardado.saveplayerdata( player,this );
+        SistemaDeGuardado.saveplayerdata(player, this);
         Debug.Log("Datasaved");
 
 
     }
-    public void pausemenu ()
+    public void pausemenu()
     {
-        if (Input.GetKey(KeyCode.Escape) && paused == false)
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
         {
             pauseui.SetActive(true);
             Time.timeScale = 0;
+            paused = true;
 
+
+        }else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseui.SetActive(false);
+            Time.timeScale = 1;
+            paused = false;
         }
+        
     }
-    
+   
+
 }
