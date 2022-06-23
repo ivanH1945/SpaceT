8ab1575aacb58bb53b1ca3e64e5be583b6bd971d
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2022-06-23 16:58:09 -0500

    segundo push

--- SpaceTraveler/Assets/Scriptsc#/IA_DE_La_Nave_Enemiga.cs
+++ SpaceTraveler/Assets/Scriptsc#/IA_DE_La_Nave_Enemiga.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,8 @@ public class IA_DE_La_Nave_Enemiga : MonoBehaviour
     [SerializeField]
     Transform movementShip;
     [SerializeField]
-    Transform rangoAgro; //rango de vista del enemigo 
-
+   float rangoAgro; //rango de vista del enemigo 
+    public float VelocidadDeMovimineto;
     Rigidbody2D RB2D;
 
     void Start()
@@ -22,5 +23,38 @@ public class IA_DE_La_Nave_Enemiga : MonoBehaviour
     {
         float Distancia_Del_Jugador = Vector2.Distance(transform.position, movementShip.position);
         Debug.Log("Distancia del Jugador :" + Distancia_Del_Jugador);
+        if(Distancia_Del_Jugador < rangoAgro)
+        {
+            CorretearJugador();
+
+        }
+        else
+        {
+            NoCorretear();
+        }
     }
+
+    private void NoCorretear()
+    {
+        
+    }
+
+    private void CorretearJugador()
+    {
+      if(transform.position.x < movementShip.position.x)
+        {
+            RB2D.velocity = new Vector2(VelocidadDeMovimineto,0f);
+        }
+      else if(transform.position.x> movementShip.position.x)
+        {
+            RB2D.velocity = new Vector2(-VelocidadDeMovimineto, 0f);
+        }
+      if(transform.position.y < movementShip.position.y) { 
+            RB2D.velocity = new Vector2(0f, VelocidadDeMovimineto);
+    }else if(transform.position.y > movementShip.position.y)
+        {
+            RB2D.velocity = new Vector2(0f, -VelocidadDeMovimineto);
+        }
+    }
+   
 }
