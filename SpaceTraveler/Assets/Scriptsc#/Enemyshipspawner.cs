2a3451fe1317c6488c99c016428afcb9efdc8ef5
ivanH1945 <ivanhernandez02@aragon.unam.mx>
2022-06-23 13:10:58 -0500

    problemas menores reseltos

--- SpaceTraveler/Assets/Scriptsc#/Enemyshipspawner.cs
+++ SpaceTraveler/Assets/Scriptsc#/Enemyshipspawner.cs
@@ -34,7 +34,8 @@ public class Enemyshipspawner : MonoBehaviour
             float randomx = Random.Range(minSpawnAreaX, maxSpawnAreaX);
             float randomy = Random.Range(minSpawnAreaY, maxSpawnAreaY);
 
-            shipformation.transform.position=new Vector3(randomx,randomy,-512);
+            shipformation.transform.position=new Vector3(randomx,randomy,-394
+                );
         }
 
     }
